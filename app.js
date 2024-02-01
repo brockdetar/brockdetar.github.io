881c03faefb73195888242b409b9d277fcc065cc
Brock DeTar <brockdetar@gmail.com>
2024-02-01 12:28:39 -0600

    Portfolio update 1.0

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-function AlertButton(){
-    alert("Hello World");
+const form = document.querySelector('form');
+
+function sendEmail() {
+    Email.send({
+        SecureToken: "c395a7ed-cf06-4c27-b2c7-d41776bc333a",
+        To : 'brockdetar@gmail.com',
+        From : "brockdetar@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    sendEmail();
+});
